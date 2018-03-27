15d76a59db7e1c141232a3d84a260015c2db6ff9
royal177 <37844468+royal177@users.noreply.github.com>
2018-03-27 22:19:05 +0530

    Add files via upload

---
+++ Rect.java
@@ -0,0 +1,32 @@
+import java.applet.*;
+
+import java.awt.*;
+
+public class Rect extends Applet
+
+{
+
+
+public void paint(Graphics g)
+
+{
+
+ g.setColor(Color.orange);
+
+ g.fillRect(120,70,100,50);
+
+ g.setColor(Color.black);
+ 
+g.drawRect(320,70,100,50);
+
+}
+
+}
+
+/*<Applet code = "Rect.class" width=600 Height=500>
+</Applet>
+*/
+
+
+
+
